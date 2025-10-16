6ce24b8315b247fd667347d966e395fd387848ea
amjadcp <amjadcp03@gmail.com>
2025-10-17 04:11:18 +0530

    add: 2 more stun servers

--- public/client.js
+++ public/client.js
@@ -19,6 +19,8 @@ let pendingCandidates = [];
 const iceServersConfig = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' }
   ]
 };
 
